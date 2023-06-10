29482d0d9193758f66a70fdbe8757e893e599c19
Jeremy Sobczak <jeremy@jeremys-mbp.lan>
2023-06-10 13:02:16 -0400

    Lights Out is now fully functional.

--- src/Board.js
+++ src/Board.js
@@ -31,20 +31,34 @@ function Board({ nrows, ncols, chanceLightStartsOn }) {
   const [board, setBoard] = useState(createBoard());
 
   /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
+  // DONE: create array-of-arrays of true/false values
   function createBoard() {
     let initialBoard = [];
-    // TODO: create array-of-arrays of true/false values
+
+    for (let i = 0; i < nrows; i++) {
+      const newRow = [];
+      for (let i = 0; i < ncols; i++) {
+        newRow.push(Math.random() <= chanceLightStartsOn);
+      }
+      initialBoard.push(newRow);
+    }
+
     return initialBoard;
   }
 
   function hasWon() {
-    // TODO: check the board in state to determine whether the player has won.
+    // DONE: check the board in state to determine whether the player has won.
+    const cellValues = board.flat();
+    // If a true value is found in flattened board array, 
+    // return false (player has not won). Otherwise
+    // return true (player has won, all lights are out).
+
+    return (cellValues.includes(Boolean(true))) ? false : true;
   }
 
   function flipCellsAround(coord) {
     setBoard(oldBoard => {
       const [y, x] = coord.split("-").map(Number);
-
       const flipCell = (y, x, boardCopy) => {
         // if this coord is actually on board, flip it
 
@@ -53,21 +67,38 @@ function Board({ nrows, ncols, chanceLightStartsOn }) {
         }
       };
 
-      // TODO: Make a (deep) copy of the oldBoard
-
-      // TODO: in the copy, flip this cell and the cells around it
-
-      // TODO: return the copy
+      // DONE: Make a (deep) copy of the oldBoard
+      const newBoard = [...oldBoard];
+      // DONE: in the copy, flip this cell and the cells around it
+      flipCell(y, x, newBoard);
+      flipCell(y+1, x, newBoard);
+      flipCell(y-1, x, newBoard);
+      flipCell(y, x+1, newBoard);
+      flipCell(y, x-1, newBoard);
+      // DONE: return the copy
+      return newBoard;
     });
   }
 
   // if the game is won, just show a winning msg & render nothing else
 
-  // TODO
+  // DONE
+  if (hasWon()) {
+    return (<h1>You Won</h1>);
+  }
 
   // make table board
 
-  // TODO
+  // DONE
+  return (
+    <div className="Board">
+      {board.map((r, rIDX) => 
+        <div>{r.map((c, cIDX) => <Cell flipCellsAroundMe={() => flipCellsAround(`${rIDX}-${cIDX}`)} isLit={c}/>)}</div>
+      )}
+    </div>
+  )
 }
+
+Board.defaultProps = { nrows: 5, ncols: 5, chanceLightStartsOn: 0.35 };
 
 export default Board;
